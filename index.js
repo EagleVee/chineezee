3f02da5630660d18bcb6aadbfa4dd2d915101bd3
EagleVee <nnavu1@gmail.com>
2020-03-05 22:34:44 +0700

    add root container

--- index.js
+++ index.js
@@ -2,8 +2,15 @@
  * @format
  */
 
-import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
+import {name as appName} from "./app.json";
+
+import {AppRegistry, YellowBox} from "react-native";
+import App from "./App";
+
+YellowBox.ignoreWarnings([
+  "Warning: componentWillMount is deprecated",
+  "Warning: componentWillReceiveProps is deprecated",
+  "Module RCTImageLoader",
+]);
 
 AppRegistry.registerComponent(appName, () => App);
