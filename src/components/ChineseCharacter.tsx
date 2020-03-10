87d59f1550f50574de171498462cbd8d4a86e5e5
EagleVee <nnavu1@gmail.com>
2020-03-10 17:00:32 +0700

    add svg chinese character

--- src/components/ChineseCharacter.tsx
+++ src/components/ChineseCharacter.tsx
@@ -1,21 +1,62 @@
-import React from "react";
-import { View, Text } from "react-native";
-import Svg from "react-native-svg";
+import React, { createRef } from "react";
+import { View, TouchableOpacity, Text } from "react-native";
+import SvgAssets from "../resources/SvgAssets";
+import { loadLocalRawResource } from "../common/AssetsHelper";
+import WebView from "react-native-webview";
+import styles from "./styles/ChineseCharacterStyle";
 
 interface Props {
   character: string;
 }
 
-function ChineseCharacter(props: Props) {
-  const { character } = props;
-  const characterUnicode = character.charCodeAt(0);
-  console.log(characterUnicode);
-  const fileName = characterUnicode + ".svg";
-  const source = "../resources/svgs/" + fileName;
+class ChineseCharacter extends React.Component<Props> {
+  webView = createRef();
+  constructor(props: Readonly<Props>) {
+    super(props);
+    this.state = {
+      xmlData: "",
+      reset: 0,
+    };
+  }
 
-  return <View style={{ flex: 1 }}>
-    <Svg />
-  </View>;
+  render() {
+    // @ts-ignore
+    const { xmlData, reset } = this.state;
+    if (xmlData.length === 0) {
+      return <View />;
+    }
+    return (
+      <View style={styles.container}>
+        <WebView
+          source={{ html: xmlData }}
+          bounces={false}
+          style={{ backgroundColor: "transparent", width: 300, height: 300 }}
+        />
+        <TouchableOpacity
+          onPress={() => {
+            this.setState({
+              reset: reset + 1,
+            });
+          }}>
+          <Text>Reset</Text>
+        </TouchableOpacity>
+      </View>
+    );
+  }
+
+  componentDidMount(): void {
+    const { character } = this.props;
+    const characterUnicode = character.charCodeAt(0);
+    // @ts-ignore
+    const file = SvgAssets[characterUnicode.toString()];
+    loadLocalRawResource(file)
+      .then(result => {
+        this.setState({
+          xmlData: result,
+        });
+      })
+      .catch(err => console.log(err));
+  }
 }
 
 export default ChineseCharacter;
---
+++ src/components/styles/ChineseCharacterStyle.ts
@@ -0,0 +1,19 @@
+import { StyleSheet } from "react-native";
+
+export default StyleSheet.create({
+  container: {
+    width: "100%",
+    height: "100%",
+    flex: 1,
+    paddingHorizontal: 20,
+    paddingVertical: 20,
+    alignItems: "center",
+  },
+  resetButton: {
+    padding: 10,
+  },
+  reset: {
+    width: 20,
+    height: 20,
+  },
+});
