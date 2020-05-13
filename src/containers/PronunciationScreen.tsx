558bb3ab1f12ee46503a3e100ecc652c83d1aba0
EagleVee <nnavu1@gmail.com>
2020-05-13 20:34:32 +0700

    add shits

--- src/containers/PronunciationScreen.tsx
+++ src/containers/PronunciationScreen.tsx
@@ -1,10 +1,10 @@
 import React, { ReactElement, useState } from "react";
-import { View, Text } from "react-native";
-import { useNavigation } from "@react-navigation/native";
+import {View, Text, TouchableOpacity} from "react-native";
 
 import styles from "./styles/StrokeOrderScreenStyle";
 import Container from "../components/Container";
 import i18n from "i18n-js";
+import {Player} from "@react-native-community/audio-toolkit";
 
 interface Props {
   navigation: any;
@@ -13,10 +13,23 @@ interface Props {
 function PronunciationScreen(props: Props): ReactElement {
   return (
     <Container style={styles.container}>
-      <View style={styles.middle}>
-        <Text style={styles.title}>{i18n.t("constructing")}</Text>
-      </View>
+      <View style={styles.middle}>{renderAudioPlayer()}</View>
     </Container>
+  );
+}
+
+function renderAudioPlayer() {
+  const playSound = () => {
+    const player = new Player("t_a4.mp3", {autoDestroy: true});
+    console.log(player);
+    player.play();
+  };
+  return (
+    <View>
+      <TouchableOpacity onPress={playSound}>
+        <Text>TOUCH TO GET THE SOUND</Text>
+      </TouchableOpacity>
+    </View>
   );
 }
 
